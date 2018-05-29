0879161176a5ece88b9ec928c4524f8a2e995cca
Samuel Fajreldines <>
2018-05-28 21:19:50 -0300

    Adding view creator to cli.php

--- core/cli/trex.php
+++ core/cli/trex.php
@@ -1,6 +1,8 @@
 <?php
 if (php_sapi_name() != "cli") die();
 
+define('DS' , DIRECTORY_SEPARATOR);
+
 function xcopy($source, $dest, $permissions = 0755){
     if (is_link($source)) { return symlink(readlink($source), $dest); }
     if (is_file($source)) { return copy($source, $dest);              }
@@ -19,7 +21,7 @@ function getDirContents($dir, &$results = array()){
     $files = scandir($dir);
 
     foreach($files as $key => $value){
-        $path = realpath($dir.DIRECTORY_SEPARATOR.$value);
+        $path = realpath($dir.DS.$value);
         if(!is_dir($path)) {
             $results[] = $path;
         } else if($value != "." && $value != "..") {
@@ -36,7 +38,8 @@ function showUsage(){
         '| T-REX FRAMEWORK |             ' . PHP_EOL .
         ' -----------------              ' . PHP_EOL .
         '* - Usage:                      ' . PHP_EOL .
-        '  > php cli.php g module NAME   ' . PHP_EOL ;
+        '  > php cli.php g module NEW-MODULE-NAME           ' . PHP_EOL .
+        '  > php cli.php g view MODULE-NAME NEW-VIEW-NAME   ' . PHP_EOL ;
 }
 function generate(){
     global $argv;
@@ -49,7 +52,7 @@ function generate(){
             $dir = TREX_ROOT. 'modules/' . $argv[3];
             if(!is_dir($dir)) {
                 mkdir($dir);
-                xcopy(TREX_ROOT.'core'.DIRECTORY_SEPARATOR.'cli'.DIRECTORY_SEPARATOR.'n-module',$dir);
+                xcopy(TREX_ROOT.'core'.DS.'cli'.DS.'n-module',$dir);
                 $items = getDirContents($dir);
 
                 foreach ($items as $item) {
@@ -66,6 +69,28 @@ function generate(){
 
             }else{
                 die('ERROR - The directory is not empty' . PHP_EOL);
+            }
+        break;
+        case 'v':
+        case 'view':
+            if(empty($argv[3])) return showUsage();
+            if(empty($argv[4])) return showUsage();
+
+            $dir     = TREX_ROOT . 'modules/' . $argv[3];
+            $new_dir = $dir . DS . 'views' . DS . $argv[4];
+
+            mkdir($new_dir);
+            var_dump(TREX_ROOT.'core'.DS.'cli'.DS.'n-module'.DS.'views'.DS.'view');
+            xcopy(
+                TREX_ROOT.'core'.DS.'cli'.DS.'n-module'.DS.'views'.DS.'view',
+                $new_dir
+            );
+
+            foreach (array('.css','.js','.php') as $item) {
+                rename(
+                    $dir . DS . 'views' . DS . $argv[4] . DS . 'view' . $item,
+                    $new_dir . DS . $argv[4] . $item
+                );
             }
         break;
         default:
